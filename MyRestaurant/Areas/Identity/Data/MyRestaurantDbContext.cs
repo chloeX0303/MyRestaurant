cadd968ae9fb2ef6578860f557dfb984b321e4c8
chloeX0303 <ac116437@avcol.school.nz>
2023-07-07 18:15:08 +1200

    Add project files.

---
+++ MyRestaurant/Areas/Identity/Data/MyRestaurantDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyRestaurant.Areas.Identity.Data;
+
+namespace MyRestaurant.Areas.Identity.Data;
+
+public class MyRestaurantDbContext : IdentityDbContext<MyRestaurantUser>
+{
+    public MyRestaurantDbContext(DbContextOptions<MyRestaurantDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new MyRestaurantUserEntityConfiguration());
+    }
+}
+
+public class MyRestaurantUserEntityConfiguration : IEntityTypeConfiguration<MyRestaurantUser>
+{
+    public void Configure(EntityTypeBuilder<MyRestaurantUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
+    }
+}
