ace004b6e0732036624695aa66aaa28de6e07456
chloeX0303 <ac116437@avcol.school.nz>
2023-07-12 15:28:10 +1200

    Authorize controllers

--- MyRestaurant/Controllers/CustomersController.cs
+++ MyRestaurant/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -12,6 +13,7 @@ using MyRestaurant.Models;
 
 namespace MyRestaurant.Controllers
 {
+    [Authorize]
     public class CustomersController : Controller
     {
         private readonly MyRestaurantDbContext _context;
