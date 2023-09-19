2279672dbcaa9c0fccb5375eeb8489abcf42643b
chloeX0303 <ac116437@avcol.school.nz>
2023-09-19 21:51:36 +1200

    change title

---
+++ dbo.Food.data.sql
@@ -0,0 +1,12 @@
+﻿﻿SET IDENTITY_INSERT [dbo].[Food] ON
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (1, N'Salt and Pepper Fried Squid', CAST(15.90 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (2, N'Roast Pork', CAST(15.90 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (3, N'Yang Chou Fried Rice', CAST(19.90 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (4, N'Chicken Fried Rice', CAST(19.90 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (5, N'Deep Fried Wonton', CAST(9.70 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (6, N'Mix Vegetable Fried Noodles', CAST(18.20 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (7, N'Roast Duck', CAST(17.00 AS Decimal(18, 2)), 1)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (8, N'Boiled Pork Dumplings', CAST(13.60 AS Decimal(18, 2)), 2)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (9, N'BBQ Pork Buns', CAST(9.00 AS Decimal(18, 2)), 2)
+INSERT INTO [dbo].[Food] ([ID], [FoodName], [FoodPrice], [Quantity]) VALUES (10, N'Sweet & Sour Pork on Rice', CAST(19.90 AS Decimal(18, 2)), 1)
+SET IDENTITY_INSERT [dbo].[Food] OFF
