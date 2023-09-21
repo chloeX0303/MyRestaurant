f61b4631fa4a09309af85a742f0e82df8b0e5faf
chloeX0303 <ac116437@avcol.school.nz>
2023-09-21 23:32:54 +1200

    data annotations

--- MyRestaurant/Models/Customer.cs
+++ MyRestaurant/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyRestaurant.Models
 {
@@ -6,14 +7,32 @@ namespace MyRestaurant.Models
     {
         public int ID { get; set; }
         [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
+        [Required(ErrorMessage = "Please enter your first name.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and symbols are not allowed.")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and symbols are not allowed.")]
         public string LastName { get; set; }
+        [Display(Name = "Home Address")]
+        [StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
+        [Required(ErrorMessage = "Please enter your home address.")]
+        [RegularExpression(@"^(?:[a-zA-Z0-9]+\s?)+[a-zA-Z0-9]+$", ErrorMessage = "Street Address not valid.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public string Address { get; set; }
         [Display(Name = "Phone Number")]
         [MaxLength(11)]
+        [MinLength(10, ErrorMessage = "Value should be greater than or equal to 10 digits")]
+        [Required(ErrorMessage = "Please enter your phone number.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer Number")]
         public string PhoneNumber { get; set; }
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Email is not valid.")]
+        [StringLength(320, ErrorMessage = "Do not enter more than 320 characters")]
+        [Required(ErrorMessage = "Please enter your email.")]
         public string Email { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
