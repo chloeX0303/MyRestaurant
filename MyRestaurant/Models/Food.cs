95239522e0a8fac17dcb767c7734011596c79fac
chloeX0303 <ac116437@avcol.school.nz>
2023-10-02 19:24:10 +1300

    data annotations for food

--- MyRestaurant/Models/Food.cs
+++ MyRestaurant/Models/Food.cs
@@ -8,10 +8,21 @@ namespace MyRestaurant.Models
     {
         public int ID { get; set; }
         [Display(Name = "Foods Name")]
+        [Required(ErrorMessage = "Please enter the name.")]
+        [StringLength(50, ErrorMessage = "Do not enter more than 50 characters")]
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+         ErrorMessage = "Numbers and symbols are not allowed.")]
         public string FoodName { get; set; }
         [Display(Name = "Food Price")]
         [Column(TypeName = "decimal(18, 2)")]
+        [Required(ErrorMessage = "Please enter the price.")]
+        [StringLength(4, ErrorMessage = "Do not enter more than 4 characters")]
+        [Range(0, 20, ErrorMessage = "Please enter valid integer Number")]
+        [MaxLength(4)]
         public decimal FoodPrice { get; set; }
+        [Required(ErrorMessage = "Please enter the quantity.")]
+        [StringLength(2, ErrorMessage = "Do not enter more than 2 characters")]
+        [Range(0, 5, ErrorMessage = "Please enter valid integer Number")]
         public int Quantity { get; set; }
         public ICollection<Order> Orders { get; set; }
     }
