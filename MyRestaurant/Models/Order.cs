8e737e12ed6afeab735bebeda2fe9aeb6013f6d4
chloeX0303 <ac116437@avcol.school.nz>
2023-10-06 22:12:31 +1300

    order

--- MyRestaurant/Models/Order.cs
+++ MyRestaurant/Models/Order.cs
@@ -8,16 +8,22 @@ namespace MyRestaurant.Models
         [Display(Name = "Order Date")]
         
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Please enter the date.")]
+        
         public DateTime OrderDate { get; set; }
         [Display(Name = "Order Time")]
         [DataType(DataType.Time)]
-        
+        [Required(ErrorMessage = "Please enter the time.")]
+
         public DateTime OrderTime { get; set; }
         [Display(Name = "Pickup Time")]
         [DataType(DataType.Time)]
-        
+        [Required(ErrorMessage = "Please enter the time.")]
+
         public DateTime PickupTime { get; set; }
+        [Required(ErrorMessage = "Please enter customerID.")]
         public int CustomerID { get; set; }
+        [Required(ErrorMessage = "Please enter foodId.")]
         public int FoodID { get; set; }
 
         public Customer Customer { get; set; }
